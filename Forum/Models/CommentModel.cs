0932cd950057f76af19440b09740a543d44ca287
Kaydon Stubbs <kaydon.stubbs@ad.snow.edu>
2019-03-07 09:54:30 -0700

    Add project files.

---
+++ Forum/Models/CommentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class CommentModel
+    {
+        public string Id { get; set; }
+        public string Creator { get; set; }
+        public string Comment { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
---
+++ Forum/Models/PostModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class PostModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
+
